973f482929e9dade768ef50b0ab225a92cb91dc2
Sahar Blueshtein <blueshtein.sahar@post.runi.ac.il>
2024-04-28 16:46:47 +0300

    Added initial endpoint for inserting, getting and deleting books

---
+++ models/books_col.py
@@ -0,0 +1,36 @@
+class BooksCollection:
+    def __init__(self):
+        self.books = {}
+        self.current_id = 1
+
+    def insert_book(self, book_data):
+        """Insert a new book and return its ID."""
+        book_id = str(self.current_id)
+        self.books[book_id]= {
+            "title": book_data['title'],
+            #"authors": book_data['authors'],
+            "ISBN": book_data['ISBN'],
+            "genre": book_data['genre'],
+            #"publisher": book_data['publisher'],
+            #"publishedDate": book_data['publishedDate'],
+            #"language": book_data['language'],
+            #"summary": book_data['summary'],
+            "id": book_id
+        }
+        self.current_id += 1
+        return book_id
+
+    def delete_book(self, book_id):
+        """Delete a book by ID."""
+        if book_id in self.books:
+            del self.books[book_id]
+            return True
+        return False
+
+    def find_book(self, book_id):
+        """Find a book by ID."""
+        return self.books.get(book_id)
+
+    def get_all_books(self):
+        """Return all books."""
+        return list(self.books.values())
---
+++ resources/books.py
@@ -0,0 +1,51 @@
+import traceback
+
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+from models.books_col import BooksCollection
+
+app = Flask(__name__)
+api = Api(app)
+books_collection = BooksCollection()  # Instance of BooksCollection
+
+# Parser to extract book fields
+books_parser = reqparse.RequestParser()
+books_parser.add_argument('title', type=str, required=True, help='Title cannot be blank!')
+books_parser.add_argument('ISBN', type=str, required=True, help='ISBN cannot be blank!')
+books_parser.add_argument('genre', type=str,
+                          choices=('Fiction', 'Children', 'Biography', 'Science', 'Science Fiction', 'Fantasy', 'Other'),
+                          required=True,
+                          help='Genre must be one of Fiction, Children, Biography, Science, Science Fiction, Fantasy, or Other')
+
+class Book(Resource):
+    def get(self, book_id):
+        try:
+            book = books_collection.find_book(book_id)
+            if book:
+                return {"message": "Book retrieved successfully.", "book": book}, 200
+            else:
+                return {"message": "Book not found.", "id": book_id}, 404
+        except Exception as e:
+            print("An error occurred:", e)
+            traceback.print_exc()
+            return {"message": "Internal server error, try later."}, 500
+
+    def post(self):
+            args = books_parser.parse_args()
+            book_data = books_collection.insert_book(args)
+            return {"message": "Book created successfully.", "book": book_data}, 201
+
+    def delete(self, book_id):
+        try:
+            if books_collection.delete_book(book_id):
+                return {"message": "Book deleted successfully.", "id": book_id}, 200
+            else:
+                return {"message": "Book not found.", "id": book_id}, 404
+        except Exception as e:
+            return {"message": "Internal server error, try later."}, 500
+
+
+api.add_resource(Book, '/book', '/book/<string:book_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
