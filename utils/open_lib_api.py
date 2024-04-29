5ba6a5730d20bbd5fe0e1232f7dcf6e67e69033e
Amir Kaplan <amirkap7@gmail.com>
2024-04-29 15:11:05 +0300

    Another small change in `open_lib_api.py`

--- utils/open_lib_api.py
+++ utils/open_lib_api.py
@@ -12,12 +12,14 @@ class OpenLibAPI:
 
         data = response.json()
         try:
+            language = []
             if not data['docs']:
                 return {"error": "No books found for the provided ISBN."}, 400
             if 'language' not in data['docs'][0]:
-                return {"error": "No language found for the provided ISBN."}, 400
-
-            language = data['docs'][0]['language']
+                language = ["missing"]
+            else:
+                language = data['docs'][0]['language']
+                language = ["missing"] if len(language) == 0 else language
             return {"language": language}, 200
         except (IndexError, KeyError) as e:
             return {"error": f"Data parsing error: {e}"}, 500
